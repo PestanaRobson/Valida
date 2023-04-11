affeb3acce650602fd04a385b16b119e3ee18321
Robson Pestana <robsonsop@gmail.com>
2023-04-11 00:49:45 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -23,6 +23,35 @@ server.use(
     '/blog/:resource/:id/show': '/:resource/:id',
   })
 );
+
+// Função para calcular dígitos verificadores do CNPJ
+function calcularDigitosCNPJ(cnpj) {
+  const pesos1 = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+  const pesos2 = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+
+  const calcularDigito = (digitos, pesos) => {
+    let soma = 0;
+    for (let i = 0; i < digitos.length; i++) {
+      soma += digitos[i] * pesos[i];
+    }
+    const resto = soma % 11;
+    return resto < 2 ? 0 : 11 - resto;
+  };
+
+  const digitos = cnpj.split("").map((digito) => parseInt(digito));
+  const digito1 = calcularDigito(digitos.slice(0, 12), pesos1);
+  const digito2 = calcularDigito(digitos.slice(0, 12).concat([digito1]), pesos2);
+
+  return [digito1, digito2];
+}
+
+// Rota personalizada para calcular dígitos verificadores do CNPJ
+server.get('/calcular-digitos-cnpj/:cnpj', (req, res) => {
+  const cnpj = req.params.cnpj;
+  const digitosVerificadores = calcularDigitosCNPJ(cnpj);
+  res.json({digitosVerificadores});
+});
+
 server.use(router);
 
 // Iniciar o servidor
