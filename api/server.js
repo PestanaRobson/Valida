296dde0055ffd0859e093eb96bbdc8d7e34ee4fc
Robson Pestana <robsonsop@gmail.com>
2023-04-20 10:00:01 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -36,75 +36,38 @@ server.use(
   })
 );
 
-// Função para calcular dígitos verificadores do CNPJ
-function calcularDigitosCNPJ(cnpj) {
-  const pesos1 = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
-  const pesos2 = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
-
-  const calcularDigito = (digitos, pesos) => {
-    let soma = 0;
-    for (let i = 0; i < digitos.length; i++) {
-      soma += digitos[i] * pesos[i];
-    }
-    const resto = soma % 11;
-    return resto < 2 ? 0 : 11 - resto;
-  };
-
-  const digitos = cnpj.split("").map((digito) => parseInt(digito));
-  const digito1 = calcularDigito(digitos.slice(0, 12), pesos1);
-  const digito2 = calcularDigito(digitos.slice(0, 12).concat([digito1]), pesos2);
-
-  return [digito1, digito2];
-}
-
-// Função para verificar a digitação correta dos CNPJs
-function validarCNPJ(cnpj) {
-  if (cnpj.length !== 14) {
-    return "CNPJ inválido";
-  }
-const digitosVerificadoresRecebidos = [parseInt(cnpj[12]), parseInt(cnpj[13])];
-const digitosVerificadoresCalculados = calcularDigitosCNPJ(cnpj.slice(0, 12));
-
-const digitadoCorretamente =
-digitosVerificadoresRecebidos[0] === digitosVerificadoresCalculados[0] &&
-digitosVerificadoresRecebidos[1] === digitosVerificadoresCalculados[1];
-
-return digitadoCorretamente;
-}
-
 async function consultarReceitaWS(cnpj) {
-const url = https://www.receitaws.com.br/v1/cnpj/${cnpj};
-const response = await axios.get(url);
+  const url = `https://www.receitaws.com.br/v1/cnpj/${cnpj}`;
+  const response = await axios.get(url);
 
-if (response.status !== 200) {
-throw new Error(Falha ao consultar a Receita WS: ${response.status});
-}
+  if (response.status !== 200) {
+    throw new Error(`Falha ao consultar a Receita WS: ${response.status}`);
+  }
 
-const data = response.data;
+  const data = response.data;
 
-return data.situacao;
+  return data.situacao;
 }
 
 // Rota personalizada para validar CNPJ e retornar dados associados
 server.get('/validar-cnpj/:cnpj', async (req, res) => {
-const cnpj = req.params.cnpj;
-const digitadoCorretamente = validarCNPJ(cnpj);
+  const cnpj = req.params.cnpj;
+  const digitadoCorretamente = validarCNPJ(cnpj);
 
-if (digitadoCorretamente) {
-const cnpjRaiz = cnpj.slice(0, 8);
-const modeloCNPJ = db.valida.find((item) => item.R === parseInt(cnpjRaiz, 10));
+  if (digitadoCorretamente) {
+    const cnpjRaiz = cnpj.slice(0, 8);
+    const modeloCNPJ = db.valida.find((item) => item.R === parseInt(cnpjRaiz, 10));
 
-if (modeloCNPJ) {
-  const situacao = await consultarReceitaWS(cnpj);
-  const mensagem = "Modelo";
-  res.json({ digitadoCorretamente, situacao, mensagem, modeloCNPJ });
-} else {
-  res.json({ digitadoCorretamente, mensagem: 'CNPJ fora do modelo' });
-}
-
-} else {
-res.json({ digitadoCorretamente, mensagem: 'CNPJ fora do modelo' });
-}
+    if (modeloCNPJ) {
+      const situacao = await consultarReceitaWS(cnpj);
+      const mensagem = "Modelo";
+      res.json({ digitadoCorretamente, situacao, mensagem, modeloCNPJ });
+    } else {
+      res.json({ digitadoCorretamente, mensagem: 'CNPJ fora do modelo' });
+    }
+  } else {
+    res.json({ digitadoCorretamente, mensagem: 'CNPJ fora do modelo' });
+  }
 });
 
 // inicio do servidor
@@ -112,7 +75,7 @@ server.use(router);
 
 // Iniciar o servidor
 server.listen(3000, () => {
-console.log('JSON Server is running');
+  console.log('JSON Server is running');
 });
 
 // Exportar a API do servidor
