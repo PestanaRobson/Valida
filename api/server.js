425f8886c77190c447eb23e0d2789c8107ad7245
Robson Pestana <robsonsop@gmail.com>
2023-04-20 10:27:46 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -36,6 +36,64 @@ server.use(
   })
 );
 
+// Configurar middlewares e rotas
+const middlewares = jsonServer.defaults();
+server.use(middlewares);
+
+server.use(
+  jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id',
+  })
+);
+
+// Função para validar CNPJ
+function validarCNPJ(cnpj) {
+  cnpj = cnpj.replace(/[^\d]+/g,'');
+
+  if(cnpj === '') {
+    return false;
+  }
+  if (cnpj.length !== 14) {
+    return false;
+  }
+  // Elimina CNPJs invalidos conhecidos
+  if (/^(\d)\1+$/.test(cnpj)) {
+    return false;
+  }
+  // Valida DVs
+  let tamanho = cnpj.length - 2;
+  let numeros = cnpj.substring(0,tamanho);
+  let digitos = cnpj.substring(tamanho);
+  let soma = 0;
+  let pos = tamanho - 7;
+  for (let i = tamanho; i >= 1; i--) {
+    soma += numeros.charAt(tamanho - i) * pos--;
+    if (pos < 2) {
+      pos = 9;
+    }
+  }
+  let resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+  if (resultado != digitos.charAt(0)) {
+    return false;
+  }
+  tamanho = tamanho + 1;
+  numeros = cnpj.substring(0,tamanho);
+  soma = 0;
+  pos = tamanho - 7;
+  for (let i = tamanho; i >= 1; i--) {
+    soma += numeros.charAt(tamanho - i) * pos--;
+    if (pos < 2) {
+      pos = 9;
+    }
+  }
+  resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+  if (resultado != digitos.charAt(1)) {
+    return false;
+  }
+  return true;
+}
+
 async function consultarReceitaWS(cnpj) {
   const url = `https://www.receitaws.com.br/v1/cnpj/${cnpj}`;
   const response = await axios.get(url);
@@ -74,9 +132,4 @@ server.get('/validar-cnpj/:cnpj', async (req, res) => {
 server.use(router);
 
 // Iniciar o servidor
-server.listen(3000, () => {
-  console.log('JSON Server is running');
-});
-
-// Exportar a API do servidor
-module.exports = server;
+server.listen
