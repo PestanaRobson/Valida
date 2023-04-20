4ad5665ee716844e5f8a4f376df2efa98b5a7d89
Robson Pestana <robsonsop@gmail.com>
2023-04-20 10:58:55 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -2,7 +2,6 @@
 const fs = require('fs');
 const zlib = require('zlib');
 const jsonServer = require('json-server');
-const axios = require('axios');
 
 // Criar o servidor JSON
 const server = jsonServer.create();
@@ -25,64 +24,6 @@ const db = { valida: combinedValida };
 // Criar o roteador usando os dados descompactados
 const router = jsonServer.router(db);
 
-// Função para validar CNPJ
-function validarCNPJ(cnpj) {
-  cnpj = cnpj.replace(/[^\d]+/g,'');
-
-  if (cnpj.length !== 14) {
-    return false;
-  }
-
-  // Elimina CNPJs invalidos conhecidos
-  if (cnpj === '00000000000000' ||
-    cnpj === '11111111111111' ||
-    cnpj === '22222222222222' ||
-    cnpj === '33333333333333' ||
-    cnpj === '44444444444444' ||
-    cnpj === '55555555555555' ||
-    cnpj === '66666666666666' ||
-    cnpj === '77777777777777' ||
-    cnpj === '88888888888888' ||
-    cnpj === '99999999999999') {
-    return false;
-  }
-
-  // Valida DVs
-  let tamanho = cnpj.length - 2;
-  let numeros = cnpj.substring(0,tamanho);
-  let digitos = cnpj.substring(tamanho);
-  let soma = 0;
-  let pos = tamanho - 7;
-  for (let i = tamanho; i >= 1; i--) {
-    soma += numeros.charAt(tamanho - i) * pos--;
-    if (pos < 2) {
-      pos = 9;
-    }
-  }
-  let resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
-  if (resultado !== parseInt(digitos.charAt(0))) {
-    return false;
-  }
-
-  tamanho = tamanho + 1;
-  numeros = cnpj.substring(0,tamanho);
-  soma = 0;
-  pos = tamanho - 7;
-  for (let i = tamanho; i >= 1; i--) {
-    soma += numeros.charAt(tamanho - i) * pos--;
-    if (pos < 2) {
-      pos = 9;
-    }
-  }
-  resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
-  if (resultado !== parseInt(digitos.charAt(1))) {
-    return false;
-  }
-
-  return true;
-}
-
-
 // Configurar middlewares e rotas
 const middlewares = jsonServer.defaults();
 
@@ -94,79 +35,45 @@ server.use(
   })
 );
 
-// Configurar middlewares e rotas
-const middlewares = jsonServer.defaults();
-server.use(middlewares);
+// Função para calcular dígitos verificadores do CNPJ
+function calcularDigitosCNPJ(cnpj) {
+  const pesos1 = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+  const pesos2 = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
 
-server.use(
-  jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id',
-  })
-);
-
-// Função para validar CNPJ
-function validarCNPJ(cnpj) {
-  cnpj = cnpj.replace(/[^\d]+/g,'');
-
-  if(cnpj === '') {
-    return false;
-  }
-  if (cnpj.length !== 14) {
-    return false;
-  }
-  // Elimina CNPJs invalidos conhecidos
-  if (/^(\d)\1+$/.test(cnpj)) {
-    return false;
-  }
-  // Valida DVs
-  let tamanho = cnpj.length - 2;
-  let numeros = cnpj.substring(0,tamanho);
-  let digitos = cnpj.substring(tamanho);
-  let soma = 0;
-  let pos = tamanho - 7;
-  for (let i = tamanho; i >= 1; i--) {
-    soma += numeros.charAt(tamanho - i) * pos--;
-    if (pos < 2) {
-      pos = 9;
+  const calcularDigito = (digitos, pesos) => {
+    let soma = 0;
+    for (let i = 0; i < digitos.length; i++) {
+      soma += digitos[i] * pesos[i];
     }
-  }
-  let resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
-  if (resultado != digitos.charAt(0)) {
-    return false;
-  }
-  tamanho = tamanho + 1;
-  numeros = cnpj.substring(0,tamanho);
-  soma = 0;
-  pos = tamanho - 7;
-  for (let i = tamanho; i >= 1; i--) {
-    soma += numeros.charAt(tamanho - i) * pos--;
-    if (pos < 2) {
-      pos = 9;
-    }
-  }
-  resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
-  if (resultado != digitos.charAt(1)) {
-    return false;
-  }
-  return true;
+    const resto = soma % 11;
+    return resto < 2 ? 0 : 11 - resto;
+  };
+
+  const digitos = cnpj.split("").map((digito) => parseInt(digito));
+  const digito1 = calcularDigito(digitos.slice(0, 12), pesos1);
+  const digito2 = calcularDigito(digitos.slice(0, 12).concat([digito1]), pesos2);
+
+  return [digito1, digito2];
 }
 
-async function consultarReceitaWS(cnpj) {
-  const url = `https://www.receitaws.com.br/v1/cnpj/${cnpj}`;
-  const response = await axios.get(url);
-
-  if (response.status !== 200) {
-    throw new Error(`Falha ao consultar a Receita WS: ${response.status}`);
+// Função para verificar a digitação correta dos CNPJs
+function validarCNPJ(cnpj) {
+  if (cnpj.length !== 14) {
+    return "CNPJ inválido";
   }
 
-  const data = response.data;
+  const digitosVerificadoresRecebidos = [parseInt(cnpj[12]), parseInt(cnpj[13])];
+  const digitosVerificadoresCalculados = calcularDigitosCNPJ(cnpj.slice(0, 12));
 
-  return data.situacao;
+  const digitadoCorretamente =
+    digitosVerificadoresRecebidos[0] === digitosVerificadoresCalculados[0] &&
+    digitosVerificadoresRecebidos[1] === digitosVerificadoresCalculados[1];
+
+  return digitadoCorretamente;
 }
 
 // Rota personalizada para validar CNPJ e retornar dados associados
-server.get('/validar-cnpj/:cnpj', async (req, res) => {
+server.get('/validar-cnpj/:cnpj', (req, res) => {
   const cnpj = req.params.cnpj;
   const digitadoCorretamente = validarCNPJ(cnpj);
 
@@ -175,9 +82,8 @@ server.get('/validar-cnpj/:cnpj', async (req, res) => {
     const modeloCNPJ = db.valida.find((item) => item.R === parseInt(cnpjRaiz, 10));
 
     if (modeloCNPJ) {
-      const situacao = await consultarReceitaWS(cnpj);
       const mensagem = "Modelo";
-      res.json({ digitadoCorretamente, situacao, mensagem, modeloCNPJ });
+      res.json({ digitadoCorretamente, mensagem, modeloCNPJ });
     } else {
       res.json({ digitadoCorretamente, mensagem: 'CNPJ fora do modelo' });
     }
@@ -186,8 +92,21 @@ server.get('/validar-cnpj/:cnpj', async (req, res) => {
   }
 });
 
+async function validarCNPJAPI(cnpj) {
+  const response = await fetch(`https://valida-teste.vercel.app/validar-cnpj/${cnpj}`);
+  const data = await response.json();
+
+  const mensagem = data.isValid ? "Digitado corretamente" : "CNPJ inválido";
+  return { isValid: data.isValid, mensagem };
+}
+
 // inicio do servidor
 server.use(router);
 
 // Iniciar o servidor
-server.listen
+server.listen(3000, () => {
+  console.log('JSON Server is running');
+});
+
+// Exportar a API do servidor
+module.exports = server;
